81b630dec0ff5044de868c4671f333e9dfaf3373
FlynnGeddes <flynn.geddes@cazoo.co.uk>
2022-12-06 18:44:15 +0000

    create and display cards
    Signed-off-by: FlynnGeddes <flynn.geddes@cazoo.co.uk>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Card } from "@mui/material";
+import { CardType, Player } from "./Classes/playerClass";
+import { UnoCard } from "./Components/card";
+import { ShowHand } from "./Components/card";
 
 function App() {
+  const player1 = new Player();
+  player1.drawCard(7);
+
+  const player2 = new Player();
+  player2.drawCard(3);
+
+  console.log(player1.hand);
+
   return (
     <div>
-      <Card sx={{ width: 100, height: 100 }}></Card>
+      <ShowHand hand={player1.hand} />
     </div>
   );
 }
