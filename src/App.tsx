55afe6b56cc7ba0103b2e905b75124a061c45064
FlynnGeddes <flynn.geddes@cazoo.co.uk>
2022-12-12 14:42:00 +0000

    Debugging to App.tsx
    Signed-off-by: FlynnGeddes <flynn.geddes@cazoo.co.uk>

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import { Button, Paper } from "@mui/material";
-import { Player } from "./Classes/playerClass";
-import { ShowHand } from "./Components/unoCard";
+import { Button } from "@mui/material";
+import { Player } from "./Classes/PlayerClass";
 
 function App() {
   const player1 = new Player();
   player1.drawCard(7);
 
-  console.log(player1.hand);
+  console.log(player1.getHand());
 
   return (
-    <Paper variant="outlined">
+    <>
       <Button
         sx={{
           height: 50,
-          width: 50,
-          backgroundColor: "hotpink",
+          width: 100,
+          backgroundColor: "black",
           color: "white",
+          justifyContent: "center",
         }}
         onClick={() => {
           player1.drawCard();
-          console.log(player1.hand);
+          console.log(player1.getHand());
         }}
       >
-        NEW CARD
+        DRAW CARD
       </Button>
-      <ShowHand hand={player1.hand} />
-    </Paper>
+      {player1.showHand()}
+    </>
   );
 }
 
