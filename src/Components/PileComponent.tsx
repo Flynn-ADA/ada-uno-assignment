190c1787d79eb9aba3cd2ac97c8a5b436c3dd0bd
FlynnGeddes <flynn.geddes@cazoo.co.uk>
2022-12-14 18:29:46 +0000

    Created pile class
    Signed-off-by: FlynnGeddes <flynn.geddes@cazoo.co.uk>

---
+++ src/Components/PileComponent.tsx
@@ -0,0 +1,18 @@
+import { CardType } from "../Classes/CardClass";
+import { RenderUnoCard } from "./UnoCardComponent";
+
+export class Pile {
+  cardInPlay: CardType;
+
+  constructor() {
+    this.cardInPlay = { type: "" };
+  }
+
+  setCard(card: CardType) {
+    this.cardInPlay = card;
+  }
+
+  renderPile() {
+    return <RenderUnoCard {...this.cardInPlay} />;
+  }
+}
