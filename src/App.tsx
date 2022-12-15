3292882ddca93d9d3ab53d39c3a8341f76d08be5
FlynnGeddes <flynn.geddes@cazoo.co.uk>
2022-12-15 17:37:25 +0000

    Added pile and allowed cards to be played
    Signed-off-by: FlynnGeddes <flynn.geddes@cazoo.co.uk>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Box, Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { useEffect, useState, useRef } from "react";
-import { UnoCard } from "./Classes/CardClass";
+import { CardType, UnoCard } from "./Classes/CardClass";
 import { Player } from "./Classes/PlayerClass";
 import { Pile } from "./Components/PileComponent";
+import { RenderUnoCard } from "./Components/UnoCardComponent";
 
 function App() {
   const player1Ref = useRef<Player>(new Player());
@@ -10,7 +11,6 @@ function App() {
   const pile = useRef<Pile>(new Pile());
   const deck = useRef<UnoCard>(new UnoCard());
   const [turnCount, setTurnCount] = useState(0);
-  let currentPlayer = "";
   // console.log("FIRST CARD ", firstCard);
 
   useEffect(() => {
@@ -19,7 +19,9 @@ function App() {
     }
 
     player1Ref.current.drawCard(7);
+    player1Ref.current.playerName = "Player 1";
     player2Ref.current.drawCard(7);
+    player2Ref.current.playerName = "Player 2";
     pile.current.setCard(deck.current.newCard());
 
     setTurnCount(turnCount + 1);
@@ -29,8 +31,9 @@ function App() {
 
   const [playerTurn, setPlayerTurn] = useState(player1Ref.current);
 
+  console.log(turnCount);
   return (
-    <>
+    <div>
       <Button
         sx={{
           height: 50,
@@ -49,40 +52,78 @@ function App() {
         Draw Card
       </Button>
 
-      <Button
-        sx={{
-          height: 50,
-          width: 100,
-          backgroundColor: "black",
-          color: "white",
-          justifyContent: "center",
-          margin: "3px",
-        }}
-        onClick={() => {
-          if (playerTurn === player1Ref.current) {
-            setPlayerTurn(player2Ref.current);
-          } else {
-            setPlayerTurn(player1Ref.current);
-          }
-        }}
+      <Grid
+        container
+        spacing={2}
+        justifyContent={"center"}
+        position={"absolute"}
+        bottom={10}
+        wrap="nowrap"
+        sx={{ overflow: "auto" }}
       >
-        Change Player
-        {currentPlayer}
-      </Button>
-      {playerTurn.getHand().length}
-      {playerTurn.showHand()}
+        {playerTurn.hand.map((card: CardType) => {
+          const cardIndex = playerTurn.hand.indexOf(card);
+          return (
+            <Grid
+              item
+              key={cardIndex}
+              sx={{
+                paddingBottom: 1,
+                transition: "top ease 0.3s",
+                position: "relative",
+                top: 0,
+                "&:hover": {
+                  top: "-10px",
+                  cursor: "pointer",
+                },
+              }}
+            >
+              <div
+                onClick={() => {
+                  if (
+                    playerTurn
+                      .playableCards(pile.current.cardInPlay)
+                      .includes(card)
+                  ) {
+                    playerTurn.hand.splice(cardIndex, 1);
+                    pile.current.setCard(card);
+                    setTurnCount(turnCount + 1);
+                    if (playerTurn === player1Ref.current) {
+                      setPlayerTurn(player2Ref.current);
+                    } else {
+                      setPlayerTurn(player1Ref.current);
+                    }
+                  }
+                }}
+              >
+                <RenderUnoCard {...card} />
+              </div>
+            </Grid>
+          );
+        })}
+      </Grid>
+
       <div
         style={{
-          border: "2px solid black",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        {playerTurn.playerName}'s turn
+      </div>
+
+      <div
+        style={{
+          display: "flex",
           justifyContent: "center",
-          position: "absolute",
-          background: "black",
-          bottom: "10",
+          alignItems: "flex-end",
+          height: "50vh",
         }}
       >
         {pile.current.renderPile()}
       </div>
-    </>
+      {/* {pile.current.setCard()} */}
+    </div>
   );
 }
 
